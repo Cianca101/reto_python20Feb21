3c7f01641cb6d0fe6d79e6db80617879bc8e9f60
Cianca101 <emmanuel.carrillo.c@gmail.com>
2021-02-20 13:42:52 -0600

    Fix identation

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,17 +1,13 @@
 ### Ejercicio 1
 
 def number_lenght(number):
-  counter = 0 # Contador en ceros
-
-  # Filtro tipo de dato y negativos
-  if number >= 0 and isinstance(number, int):
-      number_string = str(number)
-      for i in number_string:
-          counter += 1
-  else:
-      return None, "Dato no Valido"
-
-return counter
-
-number_lenght(12345)
+    counter = 0 # Contador en ceros
+    # Filtro tipo de dato y negativos
+    if number >= 0 and isinstance(number, int):
+        number_string = str(number)
+        for i in number_string:
+            counter += 1
+    else:
+        return None, "Dato no Valido"
+    return counter
 
--- ejercicio2.py
+++ ejercicio2.py
@@ -12,10 +12,9 @@ lista_mult = []
 
 def multiplos(num, leng):
     mult = 1
-
     if num >= 0 and leng >= 0 and isinstance(num, int) and isinstance(leng, int):
         for i in range(leng):
-          lista_mult.append(f'{num}x{i+1}={num*i+1}')
+            lista_mult.append(f'{num}x{i+1}={num*i+1}')
     else:
         return None, "Dato no Valido"
     return lista_mult
